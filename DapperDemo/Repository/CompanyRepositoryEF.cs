807b069097d6695ebf7acd046f2b1644243fdb5c
PeterTheHackerman <peter.s.bartos@gmail.com>
2020-09-23 17:50:15 +0100

    Add project files.

---
+++ DapperDemo/Repository/CompanyRepositoryEF.cs
@@ -0,0 +1,36 @@
+﻿using DapperDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperDemo.Repository
+{
+    public class CompanyRepositoryEF : ICompanyRepository
+    {
+        public Company Add(Company company)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Company Find(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Company> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Company Update(Company company)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
